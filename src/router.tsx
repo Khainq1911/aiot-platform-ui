3b96c1e238e0fbe45a05c04bba56097a6e6f9401
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-06-04 03:30:06 +0700

    test home page (#3)
    Co-authored-by: KhaiNguyen <khaitom1911@.dev@gmail.com>

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,10 @@
 import { RoutesConfigs } from "./configs/routes.configs";
 import AuthPage from "./pages/auth";
+import HomePage from "./pages/home";
 
-const publicRoutes = [{ path: RoutesConfigs.auth, component: AuthPage }];
+const publicRoutes = [
+  { path: RoutesConfigs.auth, component: AuthPage },
+  { path: RoutesConfigs.home, component: HomePage },
+];
 
 export { publicRoutes };
